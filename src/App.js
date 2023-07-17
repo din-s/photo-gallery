a7515607da88f71a83489ed6601f4cb1eca1888a
din-s <46292283+din-s@users.noreply.github.com>
2023-07-17 21:06:53 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="app">
+      <header>
+      <h1>Snap Shot</h1>
+      <input className="search-box" placeholder="Search Image"/>
+      <p className="trending-container"> 
+      <span className="trending-static">Trending: <span className="trending-topics">Beautiful, Cars, books</span></span>
+      </p>
+      </header>
     </div>
   );
 }
